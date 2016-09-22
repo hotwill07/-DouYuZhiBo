d5fc16ba924f3973200119ab17abf924f279beef
xuwei <xuwei@xuwei.local>
2016-09-22 14:33:12 +0800

    add 搭建项目框架 tabBar

---
+++ DYZB/DYZB/Classes/Main/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  DYZB
+//
+//  Created by xuwei on 16/9/22.
+//  Copyright © 2016年 xuwei. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addChildVC(storyName: "Home")
+        addChildVC(storyName: "Live")
+        addChildVC(storyName: "Follow")
+        addChildVC(storyName: "Profile")
+    }
+    
+    private func addChildVC(storyName: String) {
+        //1.通过storyboard获取控制器
+        let childVc = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!
+        
+        //2.将childVc添加到tabbarVC
+        addChildViewController(childVc)
+    }
+    
+    
+}
+
+
