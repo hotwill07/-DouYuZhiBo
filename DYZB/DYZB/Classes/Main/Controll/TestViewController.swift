32459884e218a0319f9c438aa85460534bb4d388
xuwei <xuwei@j1.com>
2016-11-01 14:56:41 +0800

    add： 1、一个测试类的VC

---
+++ DYZB/DYZB/Classes/Main/Controll/TestViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TestViewController.swift
+//  DYZB
+//
+//  Created by xuwei on 2016/11/1.
+//  Copyright © 2016年 xuwei. All rights reserved.
+//
+
+import UIKit
+
+/// 这是一个测试类的VC
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
