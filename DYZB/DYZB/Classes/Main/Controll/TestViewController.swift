4043e14fda005c7d9dd1aa91d047d6fb58ab1f67
xuwei <xuwei@j1.com>
2016-11-01 14:58:13 +0800

    add: 1、增加测试的方法

--- DYZB/DYZB/Classes/Main/Controll/TestViewController.swift
+++ DYZB/DYZB/Classes/Main/Controll/TestViewController.swift
@@ -13,24 +13,16 @@ class TestViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    /// 增加一个测试的方法
+    func addSomeMethod() {
+        print("这是一个测试的方法")
     }
-    */
 
 }
